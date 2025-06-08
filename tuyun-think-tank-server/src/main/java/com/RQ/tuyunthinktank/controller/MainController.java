998c1953a58e5e1337bbf1d4bdcd5187028e9c18
rich0807 <3428773947@qq.com>
2025-06-08 13:38:02 +0800

    update gitignore

--- tuyun-think-tank-server/src/main/java/com/RQ/tuyunthinktank/controller/MainController.java
+++ tuyun-think-tank-server/src/main/java/com/RQ/tuyunthinktank/controller/MainController.java
@@ -6,20 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
- * @description 测试
  * @author RQ
+ * @description 测试
  * @date 2025/5/25 下午4:25
  */
 @RestController
 @RequestMapping("/")
 public class MainController {
 
-   /**
-    * @return: com.RQ.tuyunthinktank.common.BaseResponse<java.lang.String>
-    * @author RQ
-    * @date: 2025/5/25 下午4:26
-    */
+    /**
+     * @return: com.RQ.tuyunthinktank.common.BaseResponse<java.lang.String>
+     * @author RQ
+     * @date: 2025/5/25 下午4:26
+     */
     @GetMapping("/health")
     public BaseResponse<String> health() {
         return ResultUtils.success("基础框架搭建完毕");
